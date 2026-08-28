7576b4c07d4027b0b3c321110f3474ef8f8df1b5
Ronak Khatwani <f20250634@pilani.bits-pilani.ac.in>
2026-08-28 08:37:29 +0000

    lab01 task2

--- labs/lab01/task2/ripple_adder.v
+++ labs/lab01/task2/ripple_adder.v
@@ -24,4 +24,9 @@ module ripple_adder(
 
   // TODO: your four FA_Gate instances go here.
 
+  FA_Gate FA0(a[0], b[0], cin, sum[0], c1);
+  FA_Gate FA1(a[1], b[1], c1, sum[1], c2);
+  FA_Gate FA2(a[2], b[2], c2, sum[2], c3);
+  FA_Gate FA3(a[3], b[3], c3, sum[3], cout);
+
 endmodule
