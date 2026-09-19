9f1ab408d795623fcca1e18954766e957ad90e89
Ronak Khatwani <f20250634@pilani.bits-pilani.ac.in>
2026-09-19 08:32:58 +0000

    lab02 task4

---
+++ labs/lab02/task4/and_beh_intra.v
@@ -0,0 +1,11 @@
+// and_beh_intra.v
+// Behavioral with intra-assignment delay
+
+module and_beh_intra (
+  input  a, b,
+  output reg y
+);
+  always @(*) begin
+    y = #1 a & b;
+  end
+endmodule
